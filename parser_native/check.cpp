7dffe72e4e7dcc540e6973112523c9476b3d9638
Andrew Garrett <werdna@users.mediawiki.org>
2008-08-03 00:36:27 +0000

    add some missing zsource files

---
+++ parser_native/check.cpp
@@ -0,0 +1,29 @@
+#include "afeval.h"
+#include "affunctions.h"
+
+int main( int argc, char** argv ) {
+	FilterEvaluator e;
+
+	e.reset();
+	bool result = false;
+	
+	cout << AFPData((string)"0.25").toString() << endl;
+	exit(0);
+	
+	registerBuiltinFunctions();
+	
+	for(int i=0;i<=1;i++) {
+	try {
+		e.setVar( "foo", AFPData(string("love")) );
+		result = e.evaluateFilter( "specialratio('foo;') == 0.25" );
+	} catch (AFPException* excep) {
+		printf( "Exception: %s\n", excep->what() );
+	}
+	}
+	
+	if (result) {
+		printf("Success!\n");
+	} else {
+		printf("OH NOES!\n");
+	}
+}
---
+++ parser_native/syntax_check.cpp
@@ -0,0 +1,23 @@
+#include "afeval.h"
+#include <string>
+#include <sstream>
+#include <iostream>
+
+int main( int argc, char** argv ) {
+	stringbuf ss( ios::in | ios::out );
+	
+	// Fill the stringstream
+	cin.get(ss,'\x04');
+	
+	string filter = ss.str();
+	
+	try {
+		FilterEvaluator e;
+		e.evaluateFilter( filter );
+	} catch (AFPException excep) {
+		cout << "PARSERR: " << excep.what() << endl;
+		exit(0);
+	}
+	
+	cout << "SUCCESS" << endl;
+}
