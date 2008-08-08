63ab66ade186733da1ccc0a43d8e1244717f82d8
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-08 09:37:07 +0000

    Localisation updates for extension special page aliases from Betawiki

--- AbuseFilter.alias.php
+++ AbuseFilter.alias.php
@@ -13,3 +13,22 @@ $aliases['en'] = array(
 	'AbuseLog'    => array( 'AbuseLog' ),
 	'AbuseFilter' => array( 'AbuseFilter' ),
 );
+
+/** Arabic (العربية) */
+$aliases['ar'] = array(
+	'AbuseLog'    => array( 'سجل_الإساءة' ),
+	'AbuseFilter' => array( 'فلتر_الإساءة' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$aliases['arz'] = array(
+	'AbuseLog'    => array( 'سجل_الإساءة' ),
+	'AbuseFilter' => array( 'فلتر_الإساءة' ),
+);
+
+/** Dutch (Nederlands) */
+$aliases['nl'] = array(
+	'AbuseLog'    => array( 'Misbruiklogboek' ),
+	'AbuseFilter' => array( 'Misbruikfilter' ),
+);
+
