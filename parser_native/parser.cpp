52791b24f8f486e270533c712d25681a96182be0
River Tarnell <river@users.mediawiki.org>
2008-08-08 10:36:17 +0000

    - document the parser some - make pow_expr a seperate rule with higher precedence than mult_expr - rename eq_expr/eq2_expr to ord_expr/eq_expr - match strict_real_p instead of real_p, otherwise '1 / 2' is 0.5 instead of 0

--- parser_native/parser.cpp
+++ parser_native/parser.cpp
@@ -16,6 +16,56 @@
 using namespace boost::spirit;
 using namespace phoenix;
 
+/*
+ *                    ABUSEFILTER EXPRESSION PARSER
+ *                    =============================
+ *
+ * This is the basic expression parser.  It doesn't contain any AF logic
+ * itself, but rather presents an interface for the user to add custom
+ * functions and variables.
+ *
+ * The interface to the parser is the 'expressor' class.  Use it like this:
+ *
+ *   expressor e;
+ *   e.add_variable("ONE", 1);
+ *   e.evaluate("ONE + 2");     -- returns 3 
+ * 
+ * Custom functions should have the following prototype:
+ *
+ *   datum (std::vector<afp::datum) const &args);
+ *
+ * Functions must return a value; they cannot be void.  The arguments passed to
+ * the function are stored in the 'args' array in left-to-right order.
+ *
+ * The parser implements a C-like grammar with some differences.  The following
+ * operators are available:
+ *
+ *   a & b	true if a and b are both true
+ *   a | b	true if either a or b is true
+ *   a ^ b	true if either a or b is true, but not if both are true
+ *   a + b	arithmetic
+ *   a - b
+ *   a * b
+ *   a / b
+ *   a % b
+ *   a ** b	power-of (a^b)
+ *   a in b	true if the string "b" contains the substring "a"
+ *   !a		true if a is false
+ *   (a)	same value as a
+ *   a ? b : c	if a is true, returns the value of b, otherwise c
+ *   a == b	comparison operators
+ *   a != b
+ *   a < b
+ *   a <= b
+ *   a > b
+ *   a >= b
+ *   a === b	returns true if a==b and both are the same type
+ *   a !== b	return true if a != b or they are different types
+ *
+ * The parser uses afp::datum for its variables.  This means it supports
+ * strings, ints and floats, with automatic conversion between types.
+ */
+
 namespace px = phoenix;
 
 namespace afp {
@@ -24,6 +74,10 @@ struct parse_error : std::runtime_error {
 	parse_error(char const *what) : std::runtime_error(what) {}
 };
 
+/*
+ * The parser stores the result of each grammar rule in a closure.  Most rules
+ * use the parser_closure, which simply stores a single value.
+ */
 struct parser_closure : boost::spirit::closure<parser_closure, datum>
 {
 	member1 val;
@@ -45,6 +99,11 @@ f_ternary(datum const &v, datum const &iftrue, datum const &iffalse)
 
 }
 
+/*
+ * This is the closure types for functions.  'val' stores the final result of
+ * the function call; func and args store the function object and the parsed
+ * arguments.
+ */
 struct function_closure : boost::spirit::closure<
 			  	function_closure,
 				datum,
@@ -56,6 +115,9 @@ struct function_closure : boost::spirit::closure<
 	member3 args;
 };
 
+/*
+ * The closure for the ?: operator.  Parsed as expr ? iftrue : iffalse.
+ */
 struct ternary_closure : boost::spirit::closure<
 			 ternary_closure,
 			 datum,
@@ -67,14 +129,20 @@ struct ternary_closure : boost::spirit::closure<
 	member3 iffalse;
 };
 
+/*
+ * The grammar itself.
+ */
 struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t>
 {
+	/* User-defined variables. */
 	symbols<datum> variables;
-	symbols<boost::function<datum (std::vector<datum>)> > functions;
 
 	void add_variable(std::string const &name, datum const &value) {
 		variables.add(name.c_str(), value);
 	}
+
+	/* User-defined functions. */
+	symbols<boost::function<datum (std::vector<datum>)> > functions;
 
 	void add_function(std::string const &name, boost::function<datum (std::vector<datum>)> func) {
 		functions.add(name.c_str(), func);
@@ -87,6 +155,9 @@ struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t
 
 		parser_grammar const &self_;
  
+		/*
+		 * A phoenix actor to append its argument to a container.
+		 */
 		struct push_back_impl {
 			template<typename C, typename I>
 			struct result {
@@ -101,6 +172,10 @@ struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t
 
 		phoenix::function<push_back_impl> const push_back;
 
+		/*
+		 * A phoenix actor to call a user-defined function given the
+		 * function object and arguments.
+		 */
 		struct call_function_impl {
 			template<typename F, typename A>
 			struct result {
@@ -120,21 +195,33 @@ struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t
 		, push_back(push_back_impl())
 	        , call_function(call_function_impl())
 		{
+			/*
+			 * A literal value.  Either a string, a floating
+			 * pointer number or an integer.
+			 */
 			value = 
-				  real_p[value.val = arg1]
+				  strict_real_p[value.val = arg1]
 				| int_p[value.val = arg1]
 				| confix_p('"', *c_escape_ch_p, '"')[
 					value.val = construct_<std::string>(arg1 + 1, arg2 - 1)
 				]
 				;
 
-			/* a sequence of uppercase letters is a variable */
+			/*
+			 * A variable.  If the variable is found in the
+			 * user-supplied variable list, we use that.
+			 * Otherwise, unknown variables (containing uppercase
+			 * letters and underscore only) are returned as the
+			 * empty string.
+			 */
 			variable = 
 				  self.variables[variable.val = arg1]
-				| (+upper_p)[variable.val = ""]
+				| (+ (upper_p | '_') )[variable.val = ""]
 				;
 
-			/* func(value) */
+			/*
+			 * A function call: func([arg[, arg...]]).
+			 */
 			function = 
 				  (
 					   self.functions[function.func = arg1]
@@ -144,6 +231,11 @@ struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t
 				  ) [function.val = call_function(function.func, function.args)]
 				;
 
+			/*
+			 * A basic atomic value.  Either a variable, function
+			 * or literal, or a negated expression !a, or a
+			 * parenthesised expression (a).
+			 */
 			basic =
 				  ( '(' >> tern_expr[basic.val = arg1] >> ')' )
 				| ch_p('!') >> tern_expr[basic.val = !arg1] 
@@ -152,6 +244,9 @@ struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t
 				| value[basic.val = arg1]
 				;
 
+			/*
+			 * "a in b" operator
+			 */
 			in_expr = 
 				  basic[in_expr.val = arg1]
 				>> *(
@@ -159,17 +254,32 @@ struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t
 				    )
 				;
 
-
-			mult_expr =
-				  in_expr[mult_expr.val = arg1]
-				>> *( 
-					  '*' >> in_expr[mult_expr.val *= arg1] 
-					| '/' >> in_expr[mult_expr.val /= arg1] 
-					| '%' >> in_expr[mult_expr.val %= arg1] 
-					| "**" >> in_expr[mult_expr.val = bind(&afp::pow)(mult_expr.val,arg1)]
+			/*
+			 * power-of.  This is right-associative. 
+			 */
+			pow_expr =
+				   in_expr[pow_expr.val = arg1]
+				>> !(
+					"**" >> pow_expr[pow_expr.val = bind(&afp::pow)(pow_expr.val, arg1)]
 				    )
 				;
 
+			/*
+			 * Multiplication and operators with the same
+			 * precedence.
+			 */
+			mult_expr =
+				  pow_expr[mult_expr.val = arg1]
+				>> *( 
+					  '*' >> pow_expr[mult_expr.val *= arg1] 
+					| '/' >> pow_expr[mult_expr.val /= arg1] 
+					| '%' >> pow_expr[mult_expr.val %= arg1] 
+				    )
+				;
+
+			/*
+			 * Additional and operators with the same precedence.
+			 */
 			plus_expr =
 				  mult_expr[plus_expr.val = arg1]
 				>> *( 
@@ -178,39 +288,54 @@ struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t
 				    )
 				;
 
+			/*
+			 * Ordinal comparisons and operators with the same
+			 * precedence.
+			 */
+			ord_expr  =
+				  plus_expr[ord_expr.val = arg1]
+				>> *( 
+					  "<"  >> plus_expr[ord_expr.val = ord_expr.val < arg1]
+					| ">"  >> plus_expr[ord_expr.val = ord_expr.val > arg1]
+					| "<=" >> plus_expr[ord_expr.val = ord_expr.val <= arg1]
+					| ">=" >> plus_expr[ord_expr.val = ord_expr.val >= arg1]
+				    )
+				;
+
+			/*
+			 * Equality comparisons.
+			 */
 			eq_expr =
-				  plus_expr[eq_expr.val = arg1]
+				  ord_expr[eq_expr.val = arg1]
 				>> *( 
-					  "<"  >> plus_expr[eq_expr.val = eq_expr.val < arg1]
-					| ">"  >> plus_expr[eq_expr.val = eq_expr.val > arg1]
-					| "<=" >> plus_expr[eq_expr.val = eq_expr.val <= arg1]
-					| ">=" >> plus_expr[eq_expr.val = eq_expr.val >= arg1]
+					  "=="  >> eq_expr[eq_expr.val = eq_expr.val == arg1]
+					| "!="  >> eq_expr[eq_expr.val = eq_expr.val != arg1]
+					| "===" >> eq_expr[eq_expr.val = 
+							bind(&datum::compare_with_type)(eq_expr.val, arg1)]
+					| "!==" >> eq_expr[eq_expr.val = 
+							!bind(&datum::compare_with_type)(eq_expr.val, arg1)]
 				    )
 				;
 
-			eq2_expr =
-				  eq_expr[eq2_expr.val = arg1]
-				>> *( 
-					  "=="  >> eq_expr[eq2_expr.val = eq2_expr.val == arg1]
-					| "!="  >> eq_expr[eq2_expr.val = eq2_expr.val != arg1]
-					| "===" >> eq_expr[eq2_expr.val = 
-							bind(&datum::compare_with_type)(eq2_expr.val, arg1)]
-					| "!==" >> eq_expr[eq2_expr.val = 
-							!bind(&datum::compare_with_type)(eq2_expr.val, arg1)]
-				    )
-				;
-
+			/*
+			 * Boolean expressions.
+			 */
 			bool_expr =
-				  eq2_expr[bool_expr.val = arg1]
+				  eq_expr[bool_expr.val = arg1]
 				>> *( 
-					  '&' >> eq2_expr[bool_expr.val = bool_expr.val && arg1]
-					| '|' >> eq2_expr[bool_expr.val = bool_expr.val || arg1]
-					| '^' >> eq2_expr[bool_expr.val = 
+					  '&' >> eq_expr[bool_expr.val = bool_expr.val && arg1]
+					| '|' >> eq_expr[bool_expr.val = bool_expr.val || arg1]
+					| '^' >> eq_expr[bool_expr.val = 
 							((bool_expr.val || arg1)
 							  && !(bool_expr.val && arg1)) ]
 				    )
 				;
 
+			/*
+			 * The ternary operator.  Notice this is
+			 * right-associative: a ? b ? c : d : e
+			 * is supported.
+			 */
 			tern_expr =
 				   bool_expr[tern_expr.val = arg1]
 				>> !(
@@ -222,6 +347,9 @@ struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t
 				   )
 				;
 
+			/*
+			 * The root expression type.
+			 */
 			expr = tern_expr[self.val = arg1];
 		}
 
@@ -230,7 +358,7 @@ struct parser_grammar : public grammar<parser_grammar, parser_closure::context_t
 		}
 
 		rule_t value, variable, basic, bool_expr,
-		       eq_expr, eq2_expr, mult_expr, plus_expr, in_expr, expr;
+		       ord_expr, eq_expr, pow_expr, mult_expr, plus_expr, in_expr, expr;
 		rule<ScannerT, function_closure::context_t> function;
 		rule<ScannerT, ternary_closure::context_t> tern_expr;
 	};
@@ -246,6 +374,10 @@ expressor::~expressor()
 	delete grammar_;
 }
 
+/*
+ * The user interface to evaluate an expression.  It returns the result, or
+ * throws an exception if an error occurs.
+ */
 datum
 expressor::evaluate(std::string const &filter) const
 {
