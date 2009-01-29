65bc2d1dcf5ff45f416615e0f8a77c7c18e4d7d1
Andrew Garrett <werdna@users.mediawiki.org>
2009-01-29 00:37:53 +0000

    Fix for r46500 -- use the hook so it actually works.

--- AbuseFilter.hooks.php
+++ AbuseFilter.hooks.php
@@ -165,4 +165,18 @@ class AbuseFilterHooks {
 
 		return true;
 	}
+
+	public static function onLoadExtensionSchemaUpdates() {
+		global $wgExtNewTables, $wgExtNewFields;
+		
+		// DB updates
+		$wgExtNewTables[] = array( 'abuse_filter', "$dir/abusefilter.tables.sql" );
+		$wgExtNewTables[] = array( 'abuse_filter_history', "$dir/db_patches/patch-abuse_filter_history.sql" );
+
+		$wgExtNewFields[] = array( 'abuse_filter_history', 'afh_changed_fields', "$dir/db_patches/patch-afh_changed_fields.sql" );
+		$wgExtNewFields[] = array( 'abuse_filter', 'af_deleted', "$dir/db_patches/patch-af_deleted.sql" );
+		$wgExtNewFields[] = array( 'abuse_filter', 'af_actions', "$dir/db_patches/patch-af_actions.sql" );
+
+		return true;
+	}
 }
--- AbuseFilter.php
+++ AbuseFilter.php
@@ -55,6 +55,7 @@ $wgHooks['LoadExtensionSchemaUpdates'][] = 'AbuseFilterHooks::onSchemaUpdate';
 $wgHooks['AbortDeleteQueueNominate'][] = 'AbuseFilterHooks::onAbortDeleteQueueNominate';
 $wgHooks['RecentChange_save'][] = 'AbuseFilterHooks::onRecentChangeSave';
 $wgHooks['ListDefinedTags'][] = 'AbuseFilterHooks::onListDefinedTags';
+$wgHooks['LoadExtensionSchemaUpdates'][] = 'AbuseFilterHooks::onLoadExtensionSchemaUpdates';
 
 $wgAvailableRights[] = 'abusefilter-modify';
 $wgAvailableRights[] = 'abusefilter-log-detail';
@@ -83,14 +84,6 @@ $wgAjaxExportList[] = 'AbuseFilter::ajaxCheckSyntax';
 $wgAjaxExportList[] = 'AbuseFilter::ajaxEvaluateExpression';
 $wgAjaxExportList[] = 'AbuseFilter::ajaxReAutoconfirm';
 $wgAjaxExportList[] = 'AbuseFilter::ajaxGetFilter';
-
-// DB updates
-$wgExtNewTables[] = array( 'abuse_filter', "$dir/abusefilter.tables.sql" );
-$wgExtNewTables[] = array( 'abuse_filter_history', "$dir/db_patches/patch-abuse_filter_history.sql" );
-
-$wgExtNewFields[] = array( 'abuse_filter_history', 'afh_changed_fields', "$dir/db_patches/patch-afh_changed_fields.sql" );
-$wgExtNewFields[] = array( 'abuse_filter', 'af_deleted', "$dir/db_patches/patch-af_deleted.sql" );
-$wgExtNewFields[] = array( 'abuse_filter', 'af_actions', "$dir/db_patches/patch-af_actions.sql" );
 
 // Bump the version number every time you change any of the .css/.js files
 $wgAbuseFilterStyleVersion = 3;
