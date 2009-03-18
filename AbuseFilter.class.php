34cb78e156c54b2fada9a12bad073678667ec959
Andrew Garrett <werdna@users.mediawiki.org>
2009-03-18 04:50:55 +0000

    Increase threshold for resetting counters.

--- AbuseFilter.class.php
+++ AbuseFilter.class.php
@@ -932,7 +932,7 @@ class AbuseFilter {
 
 		$storage_period = self::$statsStoragePeriod;
 		
-		if (!$total || $total > 1000) {
+		if (!$total || $total > 10000) {
 			// This is for if the total doesn't exist, or has gone past 1000.
 			//  Recreate all the keys at the same time, so they expire together.
 			$wgMemc->set( $total_key, 0, $storage_period );
