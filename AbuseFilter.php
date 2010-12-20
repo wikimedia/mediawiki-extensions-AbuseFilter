0d6bea90e21294b8532478443408c32c7cdf9abf
Daniel Friesen <dantman@users.mediawiki.org>
2010-12-20 04:05:54 +0000

    Followup r78590, apparently the code that puts unknown categories of extensions into 'other' was not implemented until MediaWiki 1.17... *sigh* So we need to include a version check on these extensions until they stop supporting pre-1.17 versions of MediaWiki.

--- AbuseFilter.php
+++ AbuseFilter.php
@@ -15,7 +15,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
  * @link http://www.mediawiki.org/wiki/Extension:AbuseFilter Documentation
  */
 
-$wgExtensionCredits['antispam'][] = array(
+$wgExtensionCredits[version_compare($wgVersion, '1.17alpha', '>=') ? 'antispam' : 'other'][] = array(
 	'path' => __FILE__,
 	'name' => 'Abuse Filter',
 	'author' => array( 'Andrew Garrett', 'River Tarnell', 'Victor Vasiliev' ),
