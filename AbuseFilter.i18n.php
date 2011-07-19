818d72b38cba826cbff9a15b63fd33d35df9e5a7
Raimond Spekking <raymond@users.mediawiki.org>
2011-07-19 20:10:05 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-07-19 19:45:00 UTC)

--- AbuseFilter.i18n.php
+++ AbuseFilter.i18n.php
@@ -9171,7 +9171,7 @@ $messages['fa'] = array(
 	'abusefilter-log-header' => 'این سیاهه خلاصه‌ای از تغییرات پالایه‌ها را نمایش می‌دهد.
 برای جزئیات کامل, بنگرید [[Special:AbuseFilter/history|فهرست]] تغییرات پالایه‌ها را.',
 	'abusefilter-log-entry-modify' => '$1 را تصحیح کرد ($2)',
-	'abusefilter-log-noresults' => 'هیچ نتیجه‌ای',
+	'abusefilter-log-noresults' => 'بدون نتیجه',
 	'abusefilter-diff-title' => 'تفاوت بین نسخه‌ها',
 	'abusefilter-diff-item' => 'مورد',
 	'abusefilter-diff-version' => 'نسخه از $1 توسط $2',
