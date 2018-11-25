610f375fbea5a86937f4d33dc8ff713f974a5f93
Huji Lee <huji.huji@gmail.com>
2018-11-24 20:16:05 -0500

    SECURITY: Remove private information from the API results
    Later, we will add a new POST request which will allow retrieving
    the private details; it will have a mandatory "reason" parameter,
    and will result in a log entry in the private details access log,
    just like the web interface.
    
    Bug: T210329
    Change-Id: Iaca492371f48fecf543268c179a651841ed12c3f
    Signed-off-by: sbassett <sbassett@wikimedia.org>
    (cherry picked from commit b5231940323768337b61ec024827ad35a9cbf755)

--- includes/api/ApiQueryAbuseLog.php
+++ includes/api/ApiQueryAbuseLog.php
@@ -51,7 +51,6 @@ class ApiQueryAbuseLog extends ApiQueryBase {
 		$fld_ids = isset( $prop['ids'] );
 		$fld_filter = isset( $prop['filter'] );
 		$fld_user = isset( $prop['user'] );
-		$fld_ip = isset( $prop['ip'] );
 		$fld_title = isset( $prop['title'] );
 		$fld_action = isset( $prop['action'] );
 		$fld_details = isset( $prop['details'] );
@@ -61,9 +60,6 @@ class ApiQueryAbuseLog extends ApiQueryBase {
 		$fld_revid = isset( $prop['revid'] );
 		$fld_wiki = $wgAbuseFilterIsCentral && isset( $prop['wiki'] );
 
-		if ( $fld_ip ) {
-			$this->checkUserRightsAny( 'abusefilter-private' );
-		}
 		if ( $fld_details ) {
 			$this->checkUserRightsAny( 'abusefilter-log-detail' );
 		}
@@ -93,7 +89,6 @@ class ApiQueryAbuseLog extends ApiQueryBase {
 		$this->addFields( 'afl_filter' );
 		$this->addFieldsIf( 'afl_id', $fld_ids );
 		$this->addFieldsIf( 'afl_user_text', $fld_user );
-		$this->addFieldsIf( 'afl_ip', $fld_ip );
 		$this->addFieldsIf( [ 'afl_namespace', 'afl_title' ], $fld_title );
 		$this->addFieldsIf( 'afl_action', $fld_action );
 		$this->addFieldsIf( 'afl_var_dump', $fld_details );
@@ -194,9 +189,6 @@ class ApiQueryAbuseLog extends ApiQueryBase {
 			if ( $fld_user ) {
 				$entry['user'] = $row->afl_user_text;
 			}
-			if ( $fld_ip ) {
-				$entry['ip'] = $row->afl_ip;
-			}
 			if ( $fld_wiki ) {
 				$entry['wiki'] = $row->afl_wiki;
 			}
@@ -285,7 +277,6 @@ class ApiQueryAbuseLog extends ApiQueryBase {
 					'ids',
 					'filter',
 					'user',
-					'ip',
 					'title',
 					'action',
 					'details',
