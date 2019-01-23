bc875d8002843bc007407300832811d4f96644a8
Daimona Eaytoy <daimona.wiki@gmail.com>
2019-01-23 16:24:02 +0100

    Fix SQL key
    When updating the abuse_filter_history table, the sequence to use is the
    one on afh_id... And we were using the af_id one since 2009.
    
    Change-Id: I3e291c780119d74be5f47e745a8de13bda85486b

--- includes/AbuseFilter.php
+++ includes/AbuseFilter.php
@@ -2518,7 +2518,7 @@ class AbuseFilter {
 		$afh_row['afh_flags'] = implode( ',', $flags );
 
 		$afh_row['afh_filter'] = $new_id;
-		$afh_row['afh_id'] = $dbw->nextSequenceValue( 'abuse_filter_af_id_seq' );
+		$afh_row['afh_id'] = $dbw->nextSequenceValue( 'abuse_filter_history_afh_id_seq' );
 
 		// Do the update
 		$dbw->insert( 'abuse_filter_history', $afh_row, __METHOD__ );
