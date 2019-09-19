2990b9a1b1fcb88ddaf773077594be946aeb2dd3
Daimona Eaytoy <daimona.wiki@gmail.com>
2019-09-19 15:59:32 +0200

    SECURITY: Avoid info leak in SpecialAbuseLog
    Deleted/suppressed usernames and summaries leak through AbuseLog.
    Temporarily hide all non-public revision from AbuseLog, until we can
    properly fix the issue.
    
    Bug: T224203
    
    Change-Id: If3d3256404d0f3dbde171831937d1a816b3e2734

--- includes/special/SpecialAbuseLog.php
+++ includes/special/SpecialAbuseLog.php
@@ -908,7 +908,9 @@ class SpecialAbuseLog extends SpecialPage {
 		$diffLink = false;
 		$isHidden = self::isHidden( $row );
 
-		if ( !self::canSeeHidden() && $isHidden === true ) {
+		// @todo T224203 Try to show the details if the revision is deleted but the AbuseLog entry
+		// is not. However, watch out to avoid showing too much stuff.
+		if ( !self::canSeeHidden() && $isHidden ) {
 			return '';
 		}
 
