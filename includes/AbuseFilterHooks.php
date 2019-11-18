f673a04026e60af7a8455dde51bc660b050f182a
Daimona Eaytoy <daimona.wiki@gmail.com>
2019-11-18 19:34:41 +0100

    Add updateVarDumps to update.php
    This shouldn't happen before the script has been tested thoroughly on
    WMF wikis with --dry-run.
    
    Bug: T213006
    Change-Id: I51425c85bd6932a5c60eb870b02195aae1c24117

--- includes/AbuseFilterHooks.php
+++ includes/AbuseFilterHooks.php
@@ -626,6 +626,7 @@ class AbuseFilterHooks {
 		$updater->addExtensionUpdate( [ [ __CLASS__, 'createAbuseFilterUser' ] ] );
 		$updater->addPostDatabaseUpdateMaintenance( 'NormalizeThrottleParameters' );
 		$updater->addPostDatabaseUpdateMaintenance( 'FixOldLogEntries' );
+		$updater->addPostDatabaseUpdateMaintenance( 'UpdateVarDumps' );
 	}
 
 	/**
