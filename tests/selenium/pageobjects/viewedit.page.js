f2759a17c922d11a4d6319de8923adde3eaa68a5
vidhi-mody <vidhimody6@gmail.com>
2020-05-21 00:04:09 +0530

    Selenium: Update to WebdriverIO v5
    Update NPM packages: webdriverio, wdio-mediawiki.
    Update ESlint configuration.
    
    Replace NPM packages:
    - wdio-mocha-framework with @wdio/mocha-framework.
    - wdio-spec-reporter with @wdio/spec-reporter.
    
    New NPM packages: @wdio/cli, @wdio/local-runner, @wdio/sync.
    
    Replace:
    - `browser.element` with `$`.
    - `chromeOptions` with `'goog:chromeOptions'`.
    - `password` with `mwPwd`.
    - `username` with `mwUser`.
    
    Bug: T253167
    Change-Id: Ia26da6d0f56398a908bd4e99ae77db603647f533

--- tests/selenium/pageobjects/viewedit.page.js
+++ tests/selenium/pageobjects/viewedit.page.js
@@ -1,13 +1,13 @@
 const Page = require( 'wdio-mediawiki/Page' );
 
 class ViewEditPage extends Page {
-	get title() { return browser.element( '#firstHeading' ); }
-	get filterId() { return browser.element( '#mw-abusefilter-edit-id .mw-input' ); }
-	get hiddenEditor() { return browser.element( '#wpFilterRules' ); }
-	get warnParams() { return browser.element( '#mw-abusefilter-warn-parameters' ); }
+	get title() { return $( '#firstHeading' ); }
+	get filterId() { return $( '#mw-abusefilter-edit-id .mw-input' ); }
+	get hiddenEditor() { return $( '#wpFilterRules' ); }
+	get warnParams() { return $( '#mw-abusefilter-warn-parameters' ); }
 	open( id ) {
 		super.openTitle( 'Special:AbuseFilter/' + id );
 	}
-	get error() { return browser.element( '.errorbox' ); }
+	get error() { return $( '.errorbox' ); }
 }
 module.exports = new ViewEditPage();
