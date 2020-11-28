95d8278b5866b6cc6a12304080d2ef5b54e83378
Reedy <reedy@wikimedia.org>
2020-11-28 03:22:11 +0000

    Clean up SchemaChangeHandler
    * Move all SQL files into db_patches (or below)
    ** Remove db type from filename
    * Remove a lot of duplicated code and simplify
    
    Change-Id: If22f2a2c46a59ac24c89ce612c74d169f053ab26

--- includes/Hooks/Handlers/SchemaChangesHandler.php
+++ includes/Hooks/Handlers/SchemaChangesHandler.php
@@ -16,135 +16,149 @@ class SchemaChangesHandler implements LoadExtensionSchemaUpdatesHook {
 	 * @throws MWException
 	 */
 	public function onLoadExtensionSchemaUpdates( $updater ) {
-		$dir = __DIR__ . '/../../../';
+		$dbType = $updater->getDB()->getType();
+		$dir = __DIR__ . "/../../../db_patches";
 
-		if ( $updater->getDB()->getType() === 'mysql' || $updater->getDB()->getType() === 'sqlite' ) {
-			if ( $updater->getDB()->getType() === 'mysql' ) {
-				$updater->addExtensionUpdate( [ 'addTable', 'abuse_filter',
-					"$dir/abusefilter.tables.sql", true ] );
-			} else {
-				$updater->addExtensionUpdate( [ 'addTable', 'abuse_filter',
-					"$dir/abusefilter.tables.sqlite.sql", true ] );
-			}
-			$updater->addExtensionTable( 'abuse_filter_history',
-				"$dir/db_patches/patch-abuse_filter_history.sql" );
+		$updater->addExtensionTable(
+			'abuse_filter',
+			"$dir/$dbType/abusefilter.sql"
+		);
 
-			$updater->addExtensionUpdate( [
-				'addField', 'abuse_filter_history', 'afh_changed_fields',
-				"$dir/db_patches/patch-afh_changed_fields.sql", true
-			] );
-			$updater->addExtensionUpdate( [ 'addField', 'abuse_filter', 'af_deleted',
-				"$dir/db_patches/patch-af_deleted.sql", true ] );
-			$updater->addExtensionUpdate( [ 'addField', 'abuse_filter', 'af_actions',
-				"$dir/db_patches/patch-af_actions.sql", true ] );
-			$updater->addExtensionUpdate( [ 'addField', 'abuse_filter', 'af_global',
-				"$dir/db_patches/patch-global_filters.sql", true ] );
-			$updater->addExtensionUpdate( [ 'addField', 'abuse_filter_log', 'afl_rev_id',
-				"$dir/db_patches/patch-afl_action_id.sql", true ] );
-			if ( $updater->getDB()->getType() === 'mysql' ) {
-				$updater->addExtensionUpdate( [ 'addIndex', 'abuse_filter_log',
-					'filter_timestamp', "$dir/db_patches/patch-fix-indexes.sql", true ] );
-			} else {
-				$updater->addExtensionUpdate( [
-					'addIndex', 'abuse_filter_log', 'afl_filter_timestamp',
-					"$dir/db_patches/patch-fix-indexes.sqlite.sql", true
-				] );
-			}
+		$updater->addExtensionTable(
+			'abuse_filter_history',
+			"$dir/$dbType/patch-abuse_filter_history.sql"
+		);
 
-			$updater->addExtensionUpdate( [ 'addField', 'abuse_filter',
-				'af_group', "$dir/db_patches/patch-af_group.sql", true ] );
-
-			$updater->addExtensionIndex(
-				'abuse_filter_log', 'afl_wiki_timestamp',
-				"$dir/db_patches/patch-global_logging_wiki-index.sql"
+		if ( $dbType === 'mysql' || $dbType === 'sqlite' ) {
+			$updater->addExtensionField(
+				'abuse_filter_history',
+				'afh_changed_fields',
+				"$dir/patch-afh_changed_fields.sql"
+			);
+			$updater->addExtensionField(
+				'abuse_filter',
+				'af_deleted',
+				"$dir/patch-af_deleted.sql"
+			);
+			$updater->addExtensionField(
+				'abuse_filter',
+				'af_actions',
+				"$dir/patch-af_actions.sql"
+			);
+			$updater->addExtensionField(
+				'abuse_filter',
+				'af_global',
+				"$dir/patch-global_filters.sql"
+			);
+			$updater->addExtensionField(
+				'abuse_filter_log',
+				'afl_rev_id',
+				"$dir/patch-afl_action_id.sql"
 			);
 
-			if ( $updater->getDB()->getType() === 'mysql' ) {
-				$updater->addExtensionUpdate( [
-					'modifyField', 'abuse_filter_log', 'afl_namespace',
-					"$dir/db_patches/patch-afl-namespace_int.sql", true
-				] );
-			} else {
-				$updater->addExtensionUpdate( [
-					'modifyField', 'abuse_filter_log', 'afl_namespace',
-					"$dir/db_patches/patch-afl-namespace_int.sqlite.sql", true
-				] );
-			}
-			if ( $updater->getDB()->getType() === 'mysql' ) {
-				$updater->addExtensionUpdate( [ 'dropField', 'abuse_filter_log',
-					'afl_log_id', "$dir/db_patches/patch-drop_afl_log_id.sql", true ] );
-			} else {
-				$updater->addExtensionUpdate( [ 'dropField', 'abuse_filter_log',
-					'afl_log_id', "$dir/db_patches/patch-drop_afl_log_id.sqlite.sql", true ] );
-			}
+			$updater->addExtensionIndex(
+				'abuse_filter_log',
+				'filter_timestamp',
+				"$dir/$dbType/patch-fix-indexes.sql"
+			);
 
-			if ( $updater->getDB()->getType() == 'mysql' ) {
-				$updater->addExtensionUpdate( [
-					'addIndex', 'abuse_filter_log', 'filter_timestamp_full',
-					"$dir/db_patches/patch-split-afl_filter.sql", true
-				] );
-			} else {
-				$updater->addExtensionUpdate( [
-					'addIndex', 'abuse_filter_log', 'filter_timestamp_full',
-					"$dir/db_patches/patch-split-afl_filter.sqlite.sql", true
-				] );
-			}
-			if ( $updater->getDB()->getType() == 'mysql' ) {
-				$updater->addExtensionUpdate( [ 'modifyField', 'abuse_filter_log', 'afl_patrolled_by',
-					"$dir/db_patches/patch-afl_change_deleted_patrolled.sql", true ] );
-			} else {
-				$updater->addExtensionUpdate( [ 'modifyField', 'abuse_filter_log', 'afl_patrolled_by',
-					"$dir/db_patches/patch-afl_change_deleted_patrolled.sqlite.sql", true ] );
-			}
-		} elseif ( $updater->getDB()->getType() === 'postgres' ) {
+			$updater->addExtensionField(
+				'abuse_filter',
+				'af_group',
+				"$dir/patch-af_group.sql"
+			);
+
+			$updater->addExtensionIndex(
+				'abuse_filter_log',
+				'afl_wiki_timestamp',
+				"$dir/patch-global_logging_wiki-index.sql"
+			);
+
+			$updater->modifyExtensionField(
+				'abuse_filter_log',
+				'afl_namespace',
+				"$dir/$dbType/patch-afl-namespace_int.sql"
+			);
+
+			$updater->dropExtensionField(
+				'abuse_filter_log',
+				'afl_log_id',
+				"$dir/$dbType/patch-drop_afl_log_id.sql"
+			);
+
+			$updater->addExtensionIndex(
+				'abuse_filter_log',
+				'filter_timestamp_full',
+				"$dir/$dbType/patch-split-afl_filter.sql"
+			);
+
+			$updater->modifyExtensionField(
+				'abuse_filter_log',
+				'afl_patrolled_by',
+				"$dir/$dbType/patch-afl_change_deleted_patrolled.sql"
+			);
+		} elseif ( $dbType === 'postgres' ) {
 			$updater->addExtensionUpdate( [
-				'addTable', 'abuse_filter', "$dir/abusefilter.tables.pg.sql", true ] );
-			$updater->addExtensionUpdate( [
-				'addTable', 'abuse_filter_history',
-				"$dir/db_patches/patch-abuse_filter_history.pg.sql", true
+				'addPgField', 'abuse_filter', 'af_actions', "TEXT NOT NULL DEFAULT ''"
 			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter', 'af_actions', "TEXT NOT NULL DEFAULT ''" ] );
+				'addPgField', 'abuse_filter', 'af_deleted', 'SMALLINT NOT NULL DEFAULT 0'
+			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter', 'af_deleted', 'SMALLINT NOT NULL DEFAULT 0' ] );
+				'addPgField', 'abuse_filter', 'af_global', 'SMALLINT NOT NULL DEFAULT 0'
+			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter', 'af_global', 'SMALLINT NOT NULL DEFAULT 0' ] );
-			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter', 'af_group', "TEXT NOT NULL DEFAULT 'default'" ] );
+				'addPgField', 'abuse_filter', 'af_group', "TEXT NOT NULL DEFAULT 'default'"
+			] );
 			$updater->addExtensionUpdate( [
 				'addPgExtIndex', 'abuse_filter', 'abuse_filter_group_enabled_id',
 				"(af_group, af_enabled, af_id)"
 			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter_history', 'afh_group', "TEXT" ] );
+				'addPgField', 'abuse_filter_history', 'afh_group', "TEXT"
+			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter_log', 'afl_wiki', 'TEXT' ] );
+				'addPgField', 'abuse_filter_log', 'afl_wiki', 'TEXT'
+			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter_log', 'afl_deleted', 'SMALLINT' ] );
+				'addPgField', 'abuse_filter_log', 'afl_deleted', 'SMALLINT'
+			] );
 			$updater->addExtensionUpdate( [
-				'setDefault', 'abuse_filter_log', 'afl_deleted', '0' ] );
+				'setDefault', 'abuse_filter_log', 'afl_deleted', '0'
+			] );
 			$updater->addExtensionUpdate( [
-				'changeNullableField', 'abuse_filter_log', 'afl_deleted', 'NOT NULL', true ] );
+				'changeNullableField', 'abuse_filter_log', 'afl_deleted', 'NOT NULL', true
+			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter_log', 'afl_patrolled_by', 'INTEGER' ] );
+				'addPgField', 'abuse_filter_log', 'afl_patrolled_by', 'INTEGER'
+			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter_log', 'afl_rev_id', 'INTEGER' ] );
+				'addPgField', 'abuse_filter_log', 'afl_rev_id', 'INTEGER'
+			] );
 			$updater->addExtensionUpdate( [
-				'changeField', 'abuse_filter_log', 'afl_filter', 'TEXT', '' ] );
+				'changeField', 'abuse_filter_log', 'afl_filter', 'TEXT', ''
+			] );
 			$updater->addExtensionUpdate( [
-				'changeField', 'abuse_filter_log', 'afl_namespace', "INTEGER", '' ] );
+				'changeField', 'abuse_filter_log', 'afl_namespace', "INTEGER", ''
+			] );
 			$updater->addExtensionUpdate( [
-				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_filter' ] );
+				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_filter'
+			] );
 			$updater->addExtensionUpdate( [
-				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_ip' ] );
+				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_ip'
+			] );
 			$updater->addExtensionUpdate( [
-				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_title' ] );
+				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_title'
+			] );
 			$updater->addExtensionUpdate( [
-				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_user' ] );
+				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_user'
+			] );
 			$updater->addExtensionUpdate( [
-				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_user_text' ] );
+				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_user_text'
+			] );
 			$updater->addExtensionUpdate( [
-				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_wiki' ] );
+				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_wiki'
+			] );
 			$updater->addExtensionUpdate( [
 				'addPgExtIndex', 'abuse_filter_log', 'abuse_filter_log_filter_timestamp',
 				'(afl_filter,afl_timestamp)'
@@ -175,18 +189,27 @@ class SchemaChangesHandler implements LoadExtensionSchemaUpdatesHook {
 				'setDefault', 'abuse_filter_log', 'afl_filter', ''
 			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter_log', 'afl_global', 'SMALLINT NOT NULL DEFAULT 0' ] );
+				'addPgField', 'abuse_filter_log', 'afl_global', 'SMALLINT NOT NULL DEFAULT 0'
+			] );
 			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter_log', 'afl_filter_id', 'INTEGER NOT NULL DEFAULT 0' ] );
+				'addPgField', 'abuse_filter_log', 'afl_filter_id', 'INTEGER NOT NULL DEFAULT 0'
+			] );
 			$updater->addExtensionUpdate( [
 				'addPgIndex', 'abuse_filter_log', 'abuse_filter_log_filter_timestamp_full',
-				'(afl_global, afl_filter_id, afl_timestamp)' ] );
-			$updater->addExtensionUpdate( [ 'setDefault', 'abuse_filter_log', 'afl_deleted', 0 ] );
+				'(afl_global, afl_filter_id, afl_timestamp)'
+			] );
 			$updater->addExtensionUpdate( [
-				'changeNullableField', 'abuse_filter_log', 'afl_deleted', 'NOT NULL', true ] );
-			$updater->addExtensionUpdate( [ 'setDefault', 'abuse_filter_log', 'afl_patrolled_by', 0 ] );
+				'setDefault', 'abuse_filter_log', 'afl_deleted', 0
+			] );
 			$updater->addExtensionUpdate( [
-				'changeNullableField', 'abuse_filter_log', 'afl_patrolled_by', 'NOT NULL', true ] );
+				'changeNullableField', 'abuse_filter_log', 'afl_deleted', 'NOT NULL', true
+			] );
+			$updater->addExtensionUpdate( [
+				'setDefault', 'abuse_filter_log', 'afl_patrolled_by', 0
+			] );
+			$updater->addExtensionUpdate( [
+				'changeNullableField', 'abuse_filter_log', 'afl_patrolled_by', 'NOT NULL', true
+			] );
 		}
 
 		$updater->addExtensionUpdate( [ [ __CLASS__, 'createAbuseFilterUser' ] ] );
