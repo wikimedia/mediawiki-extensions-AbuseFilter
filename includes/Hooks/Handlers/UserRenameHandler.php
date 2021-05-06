2c939e28a9c1a96ae45e2cc8bc386088b61e8c45
Tim Starling <tstarling@wikimedia.org>
2021-05-06 11:23:03 +1000

    Move onUserMergeAccountFields to its own file
    Sharing a handler class with UserRenameHandler means that attempting to
    merge users fails due to a missing interface if AbuseFilter and MergeUser
    are installed but Renameuser is not installed.
    
    Change-Id: I1244ab1c446840ff2648248f943d7fc784b889a7

--- includes/Hooks/Handlers/UserRenameHandler.php
+++ includes/Hooks/Handlers/UserRenameHandler.php
@@ -25,16 +25,4 @@ class UserRenameHandler implements RenameUserSQLHook {
 		];
 	}
 
-	/**
-	 * Tables that Extension:UserMerge needs to update
-	 * @todo Use new hook system once UserMerge is updated
-	 *
-	 * @param array &$updateFields
-	 */
-	public static function onUserMergeAccountFields( array &$updateFields ) {
-		$updateFields[] = [ 'abuse_filter', 'af_user', 'af_user_text' ];
-		$updateFields[] = [ 'abuse_filter_log', 'afl_user', 'afl_user_text' ];
-		$updateFields[] = [ 'abuse_filter_history', 'afh_user', 'afh_user_text' ];
-	}
-
 }
