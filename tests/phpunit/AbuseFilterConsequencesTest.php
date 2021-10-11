7cc7cfa8061f225566a99b937279ce9f60b72085
Alexander Vorwerk <alexander.vorwerk@stud.uni-goettingen.de>
2021-10-11 23:32:14 +0200

    MediaWikiTestCase -> MediaWikiIntegrationTestCase
    MediaWikiTestCase has been renamed to MediaWikiIntegrationTestCase in 1.34.
    
    Bug: T293043
    Change-Id: I6e7c5a34ae49d56a8e7b5ac7d06fa9c0283bed5e

--- tests/phpunit/AbuseFilterConsequencesTest.php
+++ tests/phpunit/AbuseFilterConsequencesTest.php
@@ -62,7 +62,7 @@ use Wikimedia\Timestamp\ConvertibleTimestamp;
  * @covers \MediaWiki\Extension\AbuseFilter\Consequences\ConsequencesLookup
  * @covers \MediaWiki\Extension\AbuseFilter\Consequences\ConsequencesExecutor
  */
-class AbuseFilterConsequencesTest extends MediaWikiTestCase {
+class AbuseFilterConsequencesTest extends MediaWikiIntegrationTestCase {
 	use AbuseFilterCreateAccountTestTrait;
 	use AbuseFilterUploadTestTrait;
 
