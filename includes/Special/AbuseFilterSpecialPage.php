52ddf41e7d157338a423acaea21a20f28680c76d
Bartosz Dziewoński <matma.rex@gmail.com>
2023-10-05 22:51:21 +0200

    Fix non-escaped messages used in page subtitles
    Reported by Phan, and also discoverable with ?uselang=x-xss.
    
    Change-Id: Ieb41d11acf543784f1cfbca5ea2272ac0bffc9a9
    (cherry picked from commit 1e54192b1b14f2c35953feb5ca770bb243a730cf)

--- includes/Special/AbuseFilterSpecialPage.php
+++ includes/Special/AbuseFilterSpecialPage.php
@@ -79,7 +79,7 @@ abstract class AbuseFilterSpecialPage extends SpecialPage {
 
 		$linkStr = $this->msg( 'parentheses' )
 			->rawParams( $this->getLanguage()->pipeList( $links ) )
-			->text();
+			->escaped();
 		$linkStr = $this->msg( 'abusefilter-topnav' )->parse() . " $linkStr";
 
 		$linkStr = Xml::tags( 'div', [ 'class' => 'mw-abusefilter-navigation' ], $linkStr );
