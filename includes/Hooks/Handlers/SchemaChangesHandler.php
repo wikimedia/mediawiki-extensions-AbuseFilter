c0a374eab89df9676fce3c196728ae8c408b1e41
Reedy <reedy@wikimedia.org>
2023-12-22 01:49:51 +0000

    Drop some ancient schema changes
    MW <= 1.34
    
    Bug: T353923
    Change-Id: Id5fd76857019c4882e31b185c8ba48ef5c1025a8

--- includes/Hooks/Handlers/SchemaChangesHandler.php
+++ includes/Hooks/Handlers/SchemaChangesHandler.php
@@ -58,20 +58,6 @@ class SchemaChangesHandler implements LoadExtensionSchemaUpdatesHook {
 		);
 
 		if ( $dbType === 'mysql' || $dbType === 'sqlite' ) {
-			// 1.34
-			$updater->dropExtensionField(
-				'abuse_filter_log',
-				'afl_log_id',
-				"$dir/$dbType/patch-drop_afl_log_id.sql"
-			);
-
-			// 1.34
-			$updater->addExtensionField(
-				'abuse_filter_log',
-				'afl_filter_id',
-				"$dir/$dbType/patch-split-afl_filter.sql"
-			);
-
 			if ( $dbType === 'mysql' ) {
 				// 1.37
 				$updater->renameExtensionIndex(
@@ -110,21 +96,6 @@ class SchemaChangesHandler implements LoadExtensionSchemaUpdatesHook {
 				"$dir/$dbType/patch-remove-afl_filter.sql"
 			);
 		} elseif ( $dbType === 'postgres' ) {
-			$updater->addExtensionUpdate( [
-				'dropPgField', 'abuse_filter_log', 'afl_log_id' ] );
-			$updater->addExtensionUpdate( [
-				'setDefault', 'abuse_filter_log', 'afl_filter', ''
-			] );
-			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter_log', 'afl_global', 'SMALLINT NOT NULL DEFAULT 0'
-			] );
-			$updater->addExtensionUpdate( [
-				'addPgField', 'abuse_filter_log', 'afl_filter_id', 'INTEGER NOT NULL DEFAULT 0'
-			] );
-			$updater->addExtensionUpdate( [
-				'addPgIndex', 'abuse_filter_log', 'abuse_filter_log_filter_timestamp_full',
-				'(afl_global, afl_filter_id, afl_timestamp)'
-			] );
 			$updater->addExtensionUpdate( [
 				'dropPgIndex', 'abuse_filter_log', 'abuse_filter_log_timestamp'
 			] );
