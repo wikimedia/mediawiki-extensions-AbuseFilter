66ee91e6c6786ae9a24039f6f06dbae131e604d9
Reedy <reedy@wikimedia.org>
2024-02-15 20:42:49 +0000

    Remove usage of writeapi userright
    Bug: T294397
    Depends-On: Ifad2edc782b36d21c8c67fecde7f011dce02c11b
    Change-Id: I9c8d384742450ab19da4ad8b172310b0df928d32

--- tests/phpunit/integration/Api/UnblockAutopromoteTest.php
+++ tests/phpunit/integration/Api/UnblockAutopromoteTest.php
@@ -64,7 +64,7 @@ class UnblockAutopromoteTest extends ApiTestCase {
 		$blockedUser = $this->mockUserAuthorityWithBlock(
 			new UserIdentityValue( 42, 'Blocked user' ),
 			$block,
-			[ 'writeapi', 'abusefilter-modify' ]
+			[ 'abusefilter-modify' ]
 		);
 
 		$store = $this->createMock( BlockAutopromoteStore::class );
