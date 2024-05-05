b8b3ef02c74131f3710d4e74060dd3d8f1721877
Kosta Harlan <kharlan@wikimedia.org>
2024-05-05 11:01:24 +0300

    phan: Update config to load ConfirmEdit
    Depends-On: Ica4c83d367f8569139188c5029ebc612f78056c0
    Change-Id: I0a67d4209f47872926806a98ae1280eefc06c4c5

--- .phan/config.php
+++ .phan/config.php
@@ -6,6 +6,7 @@ $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
 		'../../extensions/CheckUser',
+		'../../extensions/ConfirmEdit',
 		'../../extensions/Echo',
 		'../../extensions/UserMerge',
 	]
@@ -15,6 +16,7 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
 		'../../extensions/CheckUser',
+		'../../extensions/ConfirmEdit',
 		'../../extensions/Echo',
 		'../../extensions/UserMerge',
 	]
