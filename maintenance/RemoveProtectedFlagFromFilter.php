0ea7944ea3592753e91075ce4858ee9c8ba1f027
Thalia <thalia.e.chan@googlemail.com>
2024-11-25 12:02:02 +0000

    Add documentation about running RemoveProtectedFlagFromFilter
    Why:
    
    * The maintenance script RemoveProtectedFlagFromFilter was added
      to fix a mistake where a filter was 'protected' that shouldn't
      be.
    * There are certain steps that should be taken before and after
      the script is run. These are not yet documented.
    
    What:
    
    * Document that the script should only be run after checking that
      no data will be leaked.
    * Document that a note should be left for filter editors to
      explain when and why the script was run.
    
    Bug: T380290
    Change-Id: Iffe35c87782fdd499937565f1f8da62bfd36f93d

--- maintenance/RemoveProtectedFlagFromFilter.php
+++ maintenance/RemoveProtectedFlagFromFilter.php
@@ -18,6 +18,14 @@ require_once "$IP/maintenance/Maintenance.php";
  * "protected" flag from a filter, while keeping other privacy flags. This is for
  * correcting filters that were mistakenly allowed to be protected (T378551).
  *
+ * Before running this script, ensure that this filter does not use protected
+ * variables. Also ensure that removing the protected flag will not leak private
+ * data. (For example if the filter used protected variables in the past and was
+ * triggered, this could leak the data of the users who triggered it.)
+ *
+ * After running this script, make an edit in the "Notes" section of the affected
+ * filters, to explain that the script was run, and why.
+ *
  * @ingroup Maintenance
  * @since 1.44
  */
