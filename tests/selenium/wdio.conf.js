377c8b473caf16b1f4bd1bf591451e3fcaa8371e
Peter Hedenskog <phedenskog@wikimedia.org>
2026-02-19 00:20:50 +0100

    selenium: Update to wdio-mediawiki 6.3.0
    And update wdio dependencies.
    
    Bug: T417130
    Change-Id: I25285b83f23443f938cb9af79c11f30d261a68be

--- tests/selenium/wdio.conf.js
+++ tests/selenium/wdio.conf.js
@@ -1,10 +1,15 @@
 import { config as baseConfig } from 'wdio-mediawiki/wdio-defaults.conf.js';
 
-export const config = { ...baseConfig,
+export const config = { ...baseConfig
 	// Override, or add to, the setting from wdio-mediawiki.
 	// Learn more at https://webdriver.io/docs/configurationfile/
 	//
 	// Example:
-	// logLevel: 'info',
-	maxInstances: 1
+	// logLevel: 'info'
+	// To enable video recording, enable video and disable browser headless
+	// recordVideo: true,
+	// useBrowserHeadless: false,
+	//
+	// To enable screenshots on all tests, disable screenshotsOnFailureOnly
+	// screenshotsOnFailureOnly: false,
 };
