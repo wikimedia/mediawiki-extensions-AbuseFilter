0ca39f9f6f7222fb97dd3c4c34020c0ca895e526
Vaughn Walters <vwalters@wikimedia.org>
2026-04-03 15:08:04 -0500

    selenium: Upgrade wdio-mediawiki to 6.5
    Upgrade wdio-mediawiki to 6.5 and @wdio to 9.27. Also replace assert with expect
    
    Bug: T396434
    Bug: T385673
    Change-Id: Id7520061c3b7e2d420c1d11b0a31b2f19cacf8dd

--- tests/selenium/specs/editingFilters.loggedOut.js
+++ tests/selenium/specs/editingFilters.loggedOut.js
@@ -1,9 +1,8 @@
-import assert from 'node:assert/strict';
 import { viewEditPage as ViewEditPage } from '../pageobjects/viewedit.page.js';
 
 describe( 'Filter editing', () => {
 	it( 'editing interface is not visible to logged-out users', async () => {
 		await ViewEditPage.open( 'new' );
-		assert( await ViewEditPage.error.isDisplayed() );
+		await expect( ViewEditPage.error ).toBeDisplayed();
 	} );
 } );
